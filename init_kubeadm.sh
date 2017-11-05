f187841cc539f036614e642028eca21354465acc
Massimo Rofi <massimo.rofi@gmail.com>
2017-11-05 15:13:08 +0000

    fix net args bug

--- init_kubeadm.sh
+++ init_kubeadm.sh
@@ -4,12 +4,18 @@
 swapoff -a
 free -m
 kubeadm reset
+rm -Rf $HOME/.kube
+mkdir -p $HOME/.kube
+
 
 # runi kubeadm init
 kubeadm init --config kubeadm.yaml > kinit.log
 
 #setup user configs
-rm -Rf $HOME/.kube
-mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Fix kubelet bug 
+sed 's/$KUBELET_SYSTEM_PODS_ARGS//g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+systemctl daemon-reload
+systemctl restart kubelet
