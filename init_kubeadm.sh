b9c089320351ea629c63e966a0611e4557f792d9
Massimo Rofi <massimo.rofi@gmail.com>
2017-11-07 21:34:24 +0000

    remove fix

--- init_kubeadm.sh
+++ init_kubeadm.sh
@@ -16,6 +16,6 @@ cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Fix kubelet bug 
-sed 's/$KUBELET_SYSTEM_PODS_ARGS//g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-systemctl daemon-reload
-systemctl restart kubelet
+#sed 's/$KUBELET_SYSTEM_PODS_ARGS//g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+#systemctl daemon-reload
+#systemctl restart kubelet
